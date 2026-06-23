55c4621381c8cb8c4a002093a36c27f42a4b9df9
itzArka1407 <arkaprovodas2004@gmail.com>
2026-06-23 21:06:21 +0530

    Learnt usage of ImGui. Have to set up the graph for tracking the logs now

---
+++ include/AppWindow.hpp
@@ -0,0 +1,7 @@
+#pragma once
+
+class AppWindow {
+public:
+  explicit AppWindow(); // Create the app window
+  void Render();        // Render the app window o screen
+};
---
+++ include/UIWindow.hpp
@@ -0,0 +1,7 @@
+// Generates the window for the APP
+#include <imgui_impl_glfw.h>
+#include <optional>
+#pragma once
+
+std::optional<GLFWwindow *>
+generate_window(); // To generate the main window for the APP
